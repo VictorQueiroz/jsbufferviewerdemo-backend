61d379191629504433592bd360638acb9eb51cd6
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-04 01:27:24 -0300

    initial commit

---
+++ schema/schema.ts
@@ -0,0 +1,455 @@
+import {ISerializer} from "./__types__";
+import {IDeserializer} from "./__types__";
+import {IRequest} from "./__types__";
+export interface generatedFileInputParams {
+    path: string;
+    contents: string;
+}
+export function generatedFile(params: generatedFileInputParams): generatedFile {
+    return {
+        _name: 'schema.generatedFile',
+        ...params
+    };
+}
+export function encodeGeneratedFile(s: ISerializer, value: generatedFile) {
+    s.writeInt32(147338165);
+    /**
+     * encoding param: path
+     */
+    const __pv0 = value['path'];
+    s.writeString(__pv0);
+    /**
+     * encoding param: contents
+     */
+    const __pv1 = value['contents'];
+    s.writeString(__pv1);
+}
+export function decodeGeneratedFile(__d: IDeserializer): generatedFile | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== 147338165) return null;
+    let path: string;
+    let contents: string;
+    /**
+     * decoding param: path
+     */
+    path = __d.readString();
+    /**
+     * decoding param: contents
+     */
+    contents = __d.readString();
+    return {
+        _name: 'schema.generatedFile',
+        path,
+        contents
+    };
+}
+export interface generatedFile  {
+    _name: 'schema.generatedFile';
+    path: string;
+    contents: string;
+}
+export function generatedFileDefault(params: Partial<generatedFileInputParams> = {}): generatedFile {
+    return generatedFile({
+        path: "",
+        contents: "",
+        ...params
+    });
+}
+export interface generatedFilesInputParams {
+    files: ReadonlyArray<generatedFile>;
+}
+export function generatedFiles(params: generatedFilesInputParams): generatedFiles {
+    return {
+        _name: 'schema.generatedFiles',
+        ...params
+    };
+}
+export function encodeGeneratedFiles(s: ISerializer, value: generatedFiles) {
+    s.writeInt32(-1609744325);
+    /**
+     * encoding param: files
+     */
+    const __pv0 = value['files'];
+    const __l0 = __pv0.length;
+    s.writeUint32(__l0);
+    for(let __i0 = 0; __i0 < __l0; __i0++) {
+        const __v__i0 = __pv0[__i0];
+        encodeGeneratedFile(s,__v__i0);
+    }
+}
+export function decodeGeneratedFiles(__d: IDeserializer): generatedFiles | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== -1609744325) return null;
+    let files: Array<generatedFile>;
+    /**
+     * decoding param: files
+     */
+    {
+        const iindex0 = __d.readUint32();
+        const oindex0 = new Array(iindex0);
+        files = oindex0;
+        for(let index0 = 0; index0 < iindex0; index0++) {
+            const __tmp1 = decodeGeneratedFile(__d);
+            if(__tmp1 === null) return null;
+            oindex0[index0] = __tmp1;
+        }
+    }
+    return {
+        _name: 'schema.generatedFiles',
+        files
+    };
+}
+export interface generatedFiles  {
+    _name: 'schema.generatedFiles';
+    files: ReadonlyArray<generatedFile>;
+}
+export function generatedFilesDefault(params: Partial<generatedFilesInputParams> = {}): generatedFiles {
+    return generatedFiles({
+        files: [],
+        ...params
+    });
+}
+export type Request = GenerateFiles;
+export function encodeRequestTrait(s: ISerializer,value: Request) {
+    switch(value._name) {
+        case 'schema.GenerateFiles':
+            encodeGenerateFiles(s,value);
+            break;
+    }
+}
+export function decodeRequestTrait(__d: IDeserializer) {
+    const __id = __d.readInt32();
+    __d.rewindInt32();
+    let value: GenerateFiles;
+    switch(__id) {
+        case -616859713: {
+            const tmp = decodeGenerateFiles(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        default: return null;
+    }
+    return value;
+}
+export function RequestDefault() {
+    return GenerateFilesDefault();
+}
+export type Result = generatedFiles | errorInternalServerError | errorParsingError;
+export function encodeResultTrait(s: ISerializer,value: Result) {
+    switch(value._name) {
+        case 'schema.generatedFiles':
+            encodeGeneratedFiles(s,value);
+            break;
+        case 'schema.errorInternalServerError':
+            encodeErrorInternalServerError(s,value);
+            break;
+        case 'schema.errorParsingError':
+            encodeErrorParsingError(s,value);
+            break;
+    }
+}
+export function decodeResultTrait(__d: IDeserializer) {
+    const __id = __d.readInt32();
+    __d.rewindInt32();
+    let value: generatedFiles | errorInternalServerError | errorParsingError;
+    switch(__id) {
+        case -1609744325: {
+            const tmp = decodeGeneratedFiles(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        case 1176202453: {
+            const tmp = decodeErrorInternalServerError(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        case -446967748: {
+            const tmp = decodeErrorParsingError(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        default: return null;
+    }
+    return value;
+}
+export function ResultDefault() {
+    return generatedFilesDefault();
+}
+export interface GenerateFilesInputParams {
+    contents: string;
+}
+export function GenerateFiles(params: GenerateFilesInputParams): GenerateFiles {
+    return {
+        _name: 'schema.GenerateFiles',
+        ...params
+    };
+}
+export function encodeGenerateFiles(s: ISerializer, value: GenerateFiles) {
+    s.writeInt32(-616859713);
+    /**
+     * encoding param: contents
+     */
+    const __pv0 = value['contents'];
+    s.writeString(__pv0);
+}
+export function decodeGenerateFiles(__d: IDeserializer): GenerateFiles | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== -616859713) return null;
+    let contents: string;
+    /**
+     * decoding param: contents
+     */
+    contents = __d.readString();
+    return {
+        _name: 'schema.GenerateFiles',
+        contents
+    };
+}
+export interface GenerateFiles extends IRequest<generatedFiles> {
+    _name: 'schema.GenerateFiles';
+    contents: string;
+}
+export function GenerateFilesDefault(params: Partial<GenerateFilesInputParams> = {}): GenerateFiles {
+    return GenerateFiles({
+        contents: "",
+        ...params
+    });
+}
+export type Error = errorInternalServerError | errorParsingError;
+export function encodeErrorTrait(s: ISerializer,value: Error) {
+    switch(value._name) {
+        case 'schema.errorInternalServerError':
+            encodeErrorInternalServerError(s,value);
+            break;
+        case 'schema.errorParsingError':
+            encodeErrorParsingError(s,value);
+            break;
+    }
+}
+export function decodeErrorTrait(__d: IDeserializer) {
+    const __id = __d.readInt32();
+    __d.rewindInt32();
+    let value: errorInternalServerError | errorParsingError;
+    switch(__id) {
+        case 1176202453: {
+            const tmp = decodeErrorInternalServerError(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        case -446967748: {
+            const tmp = decodeErrorParsingError(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        default: return null;
+    }
+    return value;
+}
+export function ErrorDefault() {
+    return errorInternalServerErrorDefault();
+}
+export interface errorInternalServerErrorInputParams {
+}
+export function errorInternalServerError(params: errorInternalServerErrorInputParams = {}): errorInternalServerError {
+    return {
+        _name: 'schema.errorInternalServerError',
+        ...params
+    };
+}
+export function encodeErrorInternalServerError(s: ISerializer, _: errorInternalServerError) {
+    s.writeInt32(1176202453);
+}
+export function decodeErrorInternalServerError(__d: IDeserializer): errorInternalServerError | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== 1176202453) return null;
+    return {
+        _name: 'schema.errorInternalServerError',
+    };
+}
+export interface errorInternalServerError  {
+    _name: 'schema.errorInternalServerError';
+}
+export function errorInternalServerErrorDefault(params: Partial<errorInternalServerErrorInputParams> = {}): errorInternalServerError {
+    return errorInternalServerError({
+        ...params
+    });
+}
+export interface errorParsingErrorInputParams {
+    lineNumber: number;
+}
+export function errorParsingError(params: errorParsingErrorInputParams): errorParsingError {
+    return {
+        _name: 'schema.errorParsingError',
+        ...params
+    };
+}
+export function encodeErrorParsingError(s: ISerializer, value: errorParsingError) {
+    s.writeInt32(-446967748);
+    /**
+     * encoding param: lineNumber
+     */
+    const __pv0 = value['lineNumber'];
+    s.writeInt32(__pv0);
+}
+export function decodeErrorParsingError(__d: IDeserializer): errorParsingError | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== -446967748) return null;
+    let lineNumber: number;
+    /**
+     * decoding param: lineNumber
+     */
+    lineNumber = __d.readInt32();
+    return {
+        _name: 'schema.errorParsingError',
+        lineNumber
+    };
+}
+export interface errorParsingError  {
+    _name: 'schema.errorParsingError';
+    lineNumber: number;
+}
+export function errorParsingErrorDefault(params: Partial<errorParsingErrorInputParams> = {}): errorParsingError {
+    return errorParsingError({
+        lineNumber: 0,
+        ...params
+    });
+}
+export interface messageRequestInputParams {
+    requestId: number;
+    request: Request;
+}
+export function messageRequest(params: messageRequestInputParams): messageRequest {
+    return {
+        _name: 'schema.messageRequest',
+        ...params
+    };
+}
+export function encodeMessageRequest(s: ISerializer, value: messageRequest) {
+    s.writeInt32(-1960323619);
+    /**
+     * encoding param: requestId
+     */
+    const __pv0 = value['requestId'];
+    s.writeInt32(__pv0);
+    /**
+     * encoding param: request
+     */
+    const __pv1 = value['request'];
+    encodeRequestTrait(s,__pv1);
+}
+export function decodeMessageRequest(__d: IDeserializer): messageRequest | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== -1960323619) return null;
+    let requestId: number;
+    let request: Request;
+    /**
+     * decoding param: requestId
+     */
+    requestId = __d.readInt32();
+    /**
+     * decoding param: request
+     */
+    const __tmp1 = decodeRequestTrait(__d);
+    if(__tmp1 === null) return null;
+    request = __tmp1;
+    return {
+        _name: 'schema.messageRequest',
+        requestId,
+        request
+    };
+}
+export interface messageRequest  {
+    _name: 'schema.messageRequest';
+    requestId: number;
+    request: Request;
+}
+export function messageRequestDefault(params: Partial<messageRequestInputParams> = {}): messageRequest {
+    return messageRequest({
+        requestId: 0,
+        request: RequestDefault(),
+        ...params
+    });
+}
+export interface messageResponseInputParams {
+    requestId: number;
+    result: Result;
+}
+export function messageResponse(params: messageResponseInputParams): messageResponse {
+    return {
+        _name: 'schema.messageResponse',
+        ...params
+    };
+}
+export function encodeMessageResponse(s: ISerializer, value: messageResponse) {
+    s.writeInt32(-1508271331);
+    /**
+     * encoding param: requestId
+     */
+    const __pv0 = value['requestId'];
+    s.writeInt32(__pv0);
+    /**
+     * encoding param: result
+     */
+    const __pv1 = value['result'];
+    encodeResultTrait(s,__pv1);
+}
+export function decodeMessageResponse(__d: IDeserializer): messageResponse | null {
+    const __id = __d.readInt32();
+    /**
+     * decode header
+     */
+    if(__id !== -1508271331) return null;
+    let requestId: number;
+    let result: Result;
+    /**
+     * decoding param: requestId
+     */
+    requestId = __d.readInt32();
+    /**
+     * decoding param: result
+     */
+    const __tmp1 = decodeResultTrait(__d);
+    if(__tmp1 === null) return null;
+    result = __tmp1;
+    return {
+        _name: 'schema.messageResponse',
+        requestId,
+        result
+    };
+}
+export interface messageResponse  {
+    _name: 'schema.messageResponse';
+    requestId: number;
+    result: Result;
+}
+export function messageResponseDefault(params: Partial<messageResponseInputParams> = {}): messageResponse {
+    return messageResponse({
+        requestId: 0,
+        result: ResultDefault(),
+        ...params
+    });
+}
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { App } from 'uWebSockets.js';
+import {
+  errorInternalServerErrorDefault,
+  encodeErrorInternalServerError,
+  encodeMessageResponse,
+  messageResponse,
+  decodeMessageRequest,
+  generatedFiles,
+  generatedFile,
+} from '../schema/schema';
+import { Deserializer, Serializer } from 'jsbuffer/codec';
+import { FileGenerator } from 'jsbuffer/code-generator';
+import fs from 'fs';
+import path from 'path';
+
+const s = new Serializer({
+  textEncoder: new TextEncoder(),
+});
+
+App({})
+  .ws('/ws', {
+    message(ws, arrayBuffer) {
+      const message = decodeMessageRequest(
+        new Deserializer({
+          buffer: new Uint8Array(arrayBuffer),
+          textDecoder: new TextDecoder(),
+        })
+      );
+      if (message === null) {
+        encodeErrorInternalServerError(s, errorInternalServerErrorDefault());
+        ws.send(s.view());
+        return;
+      }
+      (async () => {
+        const tmpDir = await fs.promises.mkdtemp('/tmp/');
+        const tmpOutDir = await fs.promises.mkdtemp('/tmp/');
+        const tmpFile = path.resolve(tmpDir, 'file');
+        await fs.promises.writeFile(tmpFile, message.request.contents);
+        const generator = new FileGenerator(
+          {
+            path: tmpFile,
+          },
+          {
+            rootDir: tmpDir,
+            outDir: tmpOutDir,
+            textDecoder: new TextDecoder(),
+            textEncoder: new TextEncoder(),
+            indentationSize: 2,
+          }
+        );
+        s.rewind();
+        encodeMessageResponse(
+          s,
+          messageResponse({
+            requestId: message.requestId,
+            result: generatedFiles({
+              files: (await generator.generate()).map((f) =>
+                generatedFile({
+                  path: f.file,
+                  contents: f.contents,
+                })
+              ),
+            }),
+          })
+        );
+        ws.send(s.view(), true, false);
+      })().catch((reason) => {
+        console.error(reason);
+        s.rewind();
+        encodeMessageResponse(
+          s,
+          messageResponse({
+            requestId: message.requestId,
+            result: errorInternalServerErrorDefault(),
+          })
+        );
+        ws.send(s.view(), true, false);
+      });
+    },
+  })
+  .listen(4000, () => {
+    console.log('listening on port: %d', 4000);
+  });
